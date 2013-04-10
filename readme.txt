a384403d82c98f7948ca47a76f7ac1db93ad250c
chuckreynolds <chuck@rynoweb.com>
2013-04-09 20:36:23 -0700

    admin_init instead, updated to 1.2 official, cleanup, gpl3, ready for launch

--- readme.txt
+++ readme.txt
@@ -1,17 +1,17 @@
 === Remove Tools Menu ===
 Contributors: ryno267
 Donate link: http://rynoweb.com/wordpress-plugins/
-Tags: admin, tools, tool menu, tools menu, admin menu
+Tags: admin, tools, tool menu, tools menu, remove tools, admin menu
 Requires at least: 3.0
-Tested up to: 3.2
-Stable tag: trunk
+Tested up to: 3.6
+Stable tag: 1.2
 
 Super simple plugin to remove the Tools menu from WordPress Admin
 
 
 == Description ==
 
-This plugin does exactly what the title says, when activated will remove the Tools menu from your WordPress Admin navigation menu. If you need import, export, or the press-this bookmarklet again, simply deactivate the plugin, do your thing, then re-enable the Remove Tools Menu plugin to remove it again and keep it out of your way.
+This plugin does exactly what the title says, when activated will remove the Tools menu from your WordPress Admin navigation menu. If you need import, export, or get the press-this bookmarklet again, simply deactivate the plugin, do your thing, then re-enable the Remove Tools Menu plugin to remove it again and keep it out of your way. NOTE: there are certain plugins that add things to the Tools menu... those obviously won't show up either but the plugin still does what it was built to do. Have fun.
 
 
 == Installation ==
@@ -29,12 +29,14 @@ Why not? In all the WP sites I've ever had I think I've only used the Import/Exp
 
 == Upgrade Notice ==
 = 1.2 =
-No new features, just updated the way this plugin works for the new versions of WordPress. Update and carry on!
+No new features, just updated the way this plugin works for the new versions of WordPress & cleaned up a bit. Update and carry on!
 
 
 == Changelog ==
 = 1.2 =
-* Updated to admin_init action hook. Works either way but the new correct way is init. No new features, same thing :)
+* Updated to admin_init action hook. Works either way but the new correct way is init.
+* Little code cleanup and readme update
+* No new features, does the same thing :)
 
 = 1.1 =
 * Now checks if remove_menu_page function exists and uses that if it does and will default to older method if not.
--- remove-tools-menu.php
+++ remove-tools-menu.php
@@ -1,32 +1,32 @@
-<?php 
+<?php
 /*
-Plugin Name: Remove Tools Menu
-Plugin URI: http://rynoweb.com/wordpress-plugins/
-Description: Super simple plugin to remove the Tools menu from WordPress Admin - just because.
-Version: 1.2
-Author: Chuck Reynolds
-Author URI: http://chuckreynolds.us
-License: GPL2
+Plugin Name: 	Remove Tools Menu
+Plugin URI: 	http://wordpress.org/extend/plugins/remove-tools-menu/
+Description: 	Super simple plugin to remove the Tools menu from WordPress Admin - just because.
+Version: 		1.2
+Author: 		Chuck Reynolds
+Author URI: 	http://chuckreynolds.us
+License:        GPL v3
+License URI:    http://www.gnu.org/licenses/gpl-3.0.html
 */
 /*
 	Copyright 2011 WordPress Remove Tools Menu plugin (email: chuck@rynoweb.com)
-	
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as 
-	published by the Free Software Foundation.
-	
-	This program is distributed in the hope that it will be useful, 
-	but WITHOUT ANY WARRANTY; without even the implied warranty of 
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, version 3 of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+	along with this program. If not, see http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 function chuck_says_dont_be_a_tool () {
-	
+
 	if (function_exists('remove_menu_page')) {
 		remove_menu_page('tools.php');
 	}
@@ -42,5 +42,3 @@ function chuck_says_dont_be_a_tool () {
 	}
 }
 add_action('admin_init', 'chuck_says_dont_be_a_tool');
-
-?>
